a18693bcb1f9f817b32586196fbc92dbce92fdc0
MY NAME <MY EMAIL>
2014-07-28 11:32:34 -0400

    adding python modulefile

---
+++ Berksfile
@@ -0,0 +1,5 @@
+source 'https://api.berkshelf.com'
+metadata
+
+cookbook 'build-essential'
+cookbook 'python', git: 'https://github.com/kjtanaka/cookbook-python.git'
--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
 name             'environment-modules'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'FutureGrid, Indiana University'
+maintainer_email 'kj.tanaka@gmail.com'
+license          'Apache 2.0'
 description      'Installs/Configures environment-modules'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'build-essential'
+depends 'python'
